a72ac5b04807c045f7d2c690781e2ce9238b85b9
Evelina Norlin <evelina.norlin@hotmail.com>
2022-11-14 14:21:24 +0100

    fixat html kundvagn

---
+++ evelina.js
@@ -0,0 +1,9 @@
+// munkar som har mer än 0 läggs i vaukorg när kund trycker på "lägg i varukorg"
+// då visas namn, antal, pris och totalpris
+//längst ner syns summan av alla munkar
+// lägg till fält för rabattkod
+//sedan läggs även specialerbjudanden in. 
+// "till betalning-knapp"
+
+const rabattKnapp = document.querySelector("#rabatt-knapp");
+console.log(rabattKnapp)
