2dc882b4fe4b62b89c3b57e9c1593027f6635dbe
Evelina Norlin <evelina.norlin@hotmail.com>
2022-11-15 09:09:03 +0100

    js

--- evelina.js
+++ evelina.js
@@ -5,5 +5,28 @@
 //sedan läggs även specialerbjudanden in. 
 // "till betalning-knapp"
 
-const rabattKnapp = document.querySelector("#rabatt-knapp");
-console.log(rabattKnapp)
+
+
+
+
+
+
+const donutAmount = document.querySelector("#donut-amount");
+const addButton = document.querySelector("#add-button");
+const reduceButton = document.querySelector("#reduce-button");
+const toBasketBtn = document.querySelector("#add-basket");
+let donutName = "Choklad Munk";
+let donutPrice=25;
+let amount = 0;
+
+const CheckoutDonut = document.querySelector(".checkOutDonuts");
+
+
+
+addButton.addEventListener("click", function(){     amount+=1;     donutAmount.textContent = amount; } )
+reduceButton.addEventListener("click", function(){     amount-=1;     donutAmount.textContent = amount; })
+toBasketBtn.addEventListener("click", function(){     CheckoutDonut.innerHTML = `<p>${donutName}</p><p>${donutPrice} kr/st</p><p>antal: ${amount}st</p><p>tot: ${amount*donutPrice} kr</p>`; })
+
+
+
+
