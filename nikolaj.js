9a04ad4c8f6b85077563c984937ee14755fdf7f0
Evelina Norlin <evelina.norlin@hotmail.com>
2022-11-18 14:56:06 +0100

    färdigställd js

--- nikolaj.js
+++ nikolaj.js
@@ -7,7 +7,7 @@ const donutArray = [
         picture2: "images/donut_1.png",      // Munk bild 2
         name: 'Socker Munk',                 // Munkens Namn
         price: 25,                           // Munkens Pris
-        quantity: 2,                         // Antal Munkar
+        quantity: 0,                         // Antal Munkar
         category: 'Budget',                  // Munkens Kategori  Budget/Economy/Deluxe
         rating: 1                            // Munkens Rating 1-5 stjärnor
     },
@@ -158,19 +158,19 @@ generateDonuts ();   // Denna funktion skapar nya munkar i html strukturen med d
 
 
 const plusDonut = (id) => {     // Denna funktion gör så plus knappen funkar och lägger till datan i en ny array
-    
+
     let selectedDonut = id;
-  
-    let search = totalDonuts.find((x) => x.id === selectedDonut.id);
+
+    let search = donutArray.find((x) => x.id === selectedDonut.id);
 
     if (search === undefined) {
-        totalDonuts.push({
+        donutArray.push({
             id: selectedDonut.id,
-            amount: 1,
+            quantity: 1,
         })
     }
 else{
-    search.amount +=1;
+    search.quantity +=1;
 }
 // console.log(totalDonuts)
 updateDonut(selectedDonut.id)
@@ -180,21 +180,24 @@ updateDonut(selectedDonut.id)
 
 const minusDonut = (id) => {    // Denna funktion gör så minus knappen funkar och lägger till datan i en ny array
     let selectedDonut = id;
-  
-    let search = totalDonuts.find((x) => x.id === selectedDonut.id);
 
-    if (search.amount === 0) return;   // stoppar loopen om värdet blir 0
+    let search = donutArray.find((x) => x.id === selectedDonut.id);
+
+    if (search.quantity === 0) return;   // stoppar loopen om värdet blir 0
 else{
-    search.amount-=1;
+    search.quantity-=1;
 }
 updateDonut(selectedDonut.id)
 };
 
-const updateDonut = (id) => {      // Denna funktion gör så att antalet uppdateras & skrivs ut i html dokumentet.
-    let search = totalDonuts.find((x) => x.id === id);
-    console.log(search.amount);
-    document.getElementById(id).innerHTML = search.amount;
+let updateDonut = (id) => {      // Denna funktion gör så att antalet uppdateras & skrivs ut i html dokumentet.
+    let search = donutArray.find((x) => x.id === id);
+    console.log(search.quantity);
+    document.getElementById(id).innerHTML = search.quantity;
+    printCurrentDonuts();
 };
+
+
 
 
 
@@ -202,18 +205,22 @@ const updateDonut = (id) => {      // Denna funktion gör så att antalet uppdat
 
 const kundvagn = document.querySelector("#donuts-kundvagn");
 const totalpris = document.querySelector("#totalpris");
+const clearBtn = document.querySelector("#clearBtn");
 let rabattkod = document.querySelector("#rabattkod");
+const kundvagnSymbol = document.querySelector("#kundvagnSymbol");
+const timeWarning = document.querySelector("#timeWarning");
+let antalDonuts = 0;
+let totalt = 0;
+let kundkorgQuantity = "";
 
 const d = new Date();
 // skapar dagens datum
 let dd = String(d.getDate()).padStart(2, '0');
 let mm = String(d.getMonth() + 1).padStart(2, '0'); //January is 0!
 let today = mm + '/' + dd;
-console.log(today)
 
 // Skapar veckodag 1-7
 let day = d.getDay();
-console.log(day)
 
 //skapar numret på aktuell vecka
 startDate = new Date(d.getFullYear(), 0, 1);
@@ -221,27 +228,35 @@ var days = Math.floor((d - startDate) /
         (24 * 60 * 60 * 1000));
           
 var weekNumber = Math.ceil(days / 7);
-console.log(weekNumber)
 
 // skapar aktuellt klockslag
 let hours = d.getHours()
 let minutes = d.getMinutes()
 let time = hours + ":" + minutes;
-console.log (time)
 
-let totalt = 0;
+// Lägger till donuts i varukorgen
+function printCurrentDonuts(){
+    kundvagn.innerHTML = "";
+    totalt = 0;
+    antalDonuts = 0;
 
 for (let i = 0; i < donutArray.length; i ++){
 
-    totalDonutPrice = donutArray[i].price*donutArray[i].quantity;
+    // räknar ut priset på flera av samma sort
+    let totalDonutPrice = donutArray[i].price*donutArray[i].quantity;
     
     // 10% rabatt om fler än 10 av en sort
     if (donutArray[i].quantity > 10){
         totalDonutPrice = totalDonutPrice * 0.9;
     }
     
+    antalDonuts += donutArray[i].quantity; //räknar ut hur många donuts i varukorgen
     totalt += totalDonutPrice; // räknar it totalsumman av alla donuts
 
+    // Visar antal donuts och totalpris i varukorgen i menyn
+    kundvagnSymbol.innerHTML = `<i class="fa-solid fa-cart-shopping"></i>${antalDonuts} för tot ${totalt} kr`
+
+    //Skriver ut donuts i kundvagnen
     if (donutArray[i].quantity > 0) {
         kundvagn.innerHTML += `
         <ul>
@@ -253,9 +268,9 @@ for (let i = 0; i < donutArray.length; i ++){
         </ul>`
 
         totalpris.textContent = `Totalpris: ${totalt} kr`
-    }
+    } 
 }
-
+}
 
 // rabatt för tisdag och jämn vecka
 if(weekNumber % 2 == 0 && day === 2 && totalt > 25){
@@ -301,6 +316,36 @@ function rabatt(){
     }
 
 }
+
+// tömning av varukorg när man trycker på knappen
+
+clearBtn.addEventListener("click", deleteCheckout)
+
+// timers som visar varningsmeddelande i kundkorg efter 12 min 
+//och tömmer varukorg efter 15 min
+function timer(){
+    setTimeout(warningTime, 720000) // 720 000 ms = 12 min
+    setTimeout(deleteCheckout, 900000) // 900 000 ms = 15 min 
+}
+
+timer()
+
+// visar ett varningsmeddelande att det är 3 min kvar innan korgen töms
+function warningTime(){
+    alert("OBS! Beställ inom 3 minuter, annars återställs din varukorg!")
+
+}
+
+// funktion som tömmer varukorgen, antingen när man trycker på
+// töm varukorg eller när det gått 15 min
+ function deleteCheckout() {
+        for (let i = 0; i < donutArray.length; i ++) {
+        donutArray[i].quantity = 0;
+        }
+        printCurrentDonuts(); //laddar om kassan
+        generateDonuts(); //laddar om html'n
+        kundvagn.innerHTML = `<h3>Varukorgen är tom</h3>`
+        }
 
 
 
